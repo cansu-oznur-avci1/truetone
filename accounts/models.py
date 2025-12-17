20b3d61ea83781517f7fc1b0889e2862a5c10e4f
Cansu Oznur AVCI <canozavci0203@gmail.com>
2025-12-17 17:52:54 +0300

    Initial Django project setup with custom user authentication

---
+++ accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('owner', 'Service Owner'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+
+    def __str__(self):
+        return self.username
