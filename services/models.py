68a3eaf6105b5e7bb48f132f8bdb32c9d3dd96b6
Cansu Oznur AVCI <canozavci0203@gmail.com>
2025-12-17 18:51:38 +0300

    Add Service model and admin integration

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='services'
+    )
+
+    def __str__(self):
+        return self.name
+
