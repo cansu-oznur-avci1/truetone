3f99955d125066486d0c95b75772b9ead6604f1e
Cansu Oznur AVCI <canozavci0203@gmail.com>
2025-12-20 22:53:27 +0300

    Service listing is complete, URL namespace errors have been resolved, and integration is successful.

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 from .views import submit_feedback
 
+app_name = 'feedback'
+
 urlpatterns = [
-    path('submit/', submit_feedback, name='submit_feedback'),
+    path('submit/<int:service_id>/', submit_feedback, name='submit_feedback'),
     path('dashboard/', views.service_owner_dashboard, name='service_owner_dashboard'),
 ]
--- truetone/urls.py
+++ truetone/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
+from services import views
+
 def home(request):
     return render(request, "base.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path('accounts/', include('accounts.urls')), 
-    path('feedback/', include('feedback.urls')),  
+    path('feedback/', include('feedback.urls', namespace='feedback')),  
     path('services/', include('services.urls')),  
-    path("", home, name="home"),
+    path("", views.service_list, name="home"),
 ]
